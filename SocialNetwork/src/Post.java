67b038ed47746da1c81013de00fc4d6fdf6f6074
Safe6er69 <kurtajleon533@gmail.com>
2023-11-21 17:12:24 +0100

    String erstellt

--- SocialNetwork/src/Post.java
+++ SocialNetwork/src/Post.java
@@ -1,5 +1,12 @@
 public abstract class Post {
 
+    private String username;
+    private String filename;
+    private String caption;
+    private long timestamp;
+    private int likes;
+    private ArrayList<String> comments;
+
 
 
 }
