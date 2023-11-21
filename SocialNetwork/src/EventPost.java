efab5290145f94a5e5ccf0c3bf966f1fdf1a0f00
Safe6er69 <kurtajleon533@gmail.com>
2023-11-21 14:21:53 +0100

    Habe EventPost.java erstellt und einen public Class script erstellt

---
+++ SocialNetwork/src/EventPost.java
@@ -0,0 +1,11 @@
+import java.util.Arraylist;
+
+public class EventPost {
+
+    private String author;
+    private long timeStamp;
+    private int pages;
+    private int likes;
+    private ArrayList<String> comments;
+
+}
