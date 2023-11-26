081b4ab9d7558f6352975a85f425129684c74099
Safe6er69 <kurtajleon533@gmail.com>
2023-11-26 21:10:45 +0100

    Habe sie Strings im Post hinzugefügt

--- SocialNetwork/src/Post.java
+++ SocialNetwork/src/Post.java
@@ -7,6 +7,42 @@ public abstract class Post {
     private int likes;
     private ArrayList<String> comments;
 
+    public EventPost(String author, String text) {
 
+        username= author;
+        message = text;
+        timestamp = System.currentTimeMillis();
+        likes = 0;
+        comments = new ArrayList<>();
+    }
+
+    public void addComment(String text) {
+        comments.add(text);
+    }
+
+    public String getImageFile() {
+        return filename;
+    }
+
+    public long getTimeStamp() {
+        return timestamp;
+    }
+
+    private String timeString() {
+
+        long current = System.currentTimeMillis();
+        long pastMillis = current - timestamp;
+        long seconds = pastMillis/1000;
+        long minutes = seconds/60;
+        long hours = minutes/60;
+        if (minutes > 0) {
+            return minutes + " minutes ago";
+        } else if(hours > 0){
+            return hours + " hours ago";
+        } else {
+            return "a few seconds ago";
+        }
+
+    }
 
 }
