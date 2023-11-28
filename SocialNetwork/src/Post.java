c6759a58620e9dbdde050a4feef79a82fc10c365
Safe6er69 <kurtajleon533@gmail.com>
2023-11-28 14:21:28 +0100

    Attribute angepasst

--- SocialNetwork/src/Post.java
+++ SocialNetwork/src/Post.java
@@ -3,8 +3,6 @@ import java.util.Arraylist;
 public abstract class Post {
 
     private String username;
-    private String filename;
-    private String caption;
     private long timestamp;
     private int likes;
     private ArrayList<String> comments;
