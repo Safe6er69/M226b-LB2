1d67175c56cc5f70a9fb3d4dd916cb29d9a37811
Safe6er69 <kurtajleon533@gmail.com>
2023-12-05 15:56:37 +0100

    Added doMeasurements for the two classes

--- Abstrakte Klassen/Sensor/Thermo2000lmpl.java
+++ Abstrakte Klassen/Sensor/Thermo2000lmpl.java
@@ -1,3 +1,10 @@
 public class Thermo2000Impl extends TemperaturSensor{
 
+    public void doMeasurement() {
+        double max = 1.05;
+        double min = 0.5;
+        double range = max - min;
+        measurementValue = (Math.random() * range) + min;
+
+    }
 }
