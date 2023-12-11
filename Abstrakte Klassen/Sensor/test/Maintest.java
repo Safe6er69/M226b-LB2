e4eb88ec203023d16de25c2e216472c111aea63b
lkurtaj <kurtajleon533@gmail.com>
2023-12-11 13:42:20 +0100

    Created test-class

---
+++ Abstrakte Klassen/Sensor/test/Maintest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        // Arrange - Set up any necessary objects or variables
+        // For example, you might use mocking frameworks to simulate behavior.
+
+        // Act - Call the method or perform the action you want to test
+        Main.main(new String[]{});
+
+        // Assert - Check if the expected results match the actual results
+        // Add assertions based on the behavior you expect from the Main class
+        // For example, you might check if the CSV file is created after running the main method.
+        // You may need to use assertions from a testing library or implement your own checks.
+
+        // For simplicity, this example includes a placeholder assertion
+        assertTrue(true); // Replace with actual assertions
+    }
