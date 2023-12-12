b604ba63abea42a59fad32845e0dffcf93ed368e
Safe6er69 <kurtajleon533@gmail.com>
2023-12-12 14:49:42 +0100

    added value to pressuresensor

--- Abstrakte Klassen/Sensor/main/PressureSensor.java
+++ Abstrakte Klassen/Sensor/main/PressureSensor.java
@@ -1,7 +1,7 @@
 public abstract class PressureSensor extends Sensor {
 
     private String unit;
-    protected double measurementValue;
+    protected double measurementValue = 69;
 
     public PressureSensor(String unit, double value, String name) {
         super();
