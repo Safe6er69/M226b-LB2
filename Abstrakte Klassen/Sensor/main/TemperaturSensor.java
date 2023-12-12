4d325bdf114dd8c93f642cd2da5f9cdc8d0fa11e
Safe6er69 <kurtajleon533@gmail.com>
2023-12-12 14:34:55 +0100

    gave measurementvalue a value

--- Abstrakte Klassen/Sensor/main/TemperaturSensor.java
+++ Abstrakte Klassen/Sensor/main/TemperaturSensor.java
@@ -3,7 +3,10 @@ public abstract class TemperaturSensor extends Sensor {
     private String unit;
     protected double measurementValue = 69;
 
-    public TemperaturSensor(String unit, double value, String name) { super(); }
+    public TemperaturSensor(String unit, double value, String name){
+        super(name, value, unit);
+
+    }
 
     public double getValue() { return measurementValue; }
 
