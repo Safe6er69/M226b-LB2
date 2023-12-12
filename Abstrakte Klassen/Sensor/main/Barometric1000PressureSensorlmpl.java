2c92653a2287ce1ad34f75cf0d123a87597d164b
Safe6er69 <kurtajleon533@gmail.com>
2023-12-12 09:13:21 +0100

    changed main-directory to source

--- Abstrakte Klassen/Sensor/main/Barometric1000PressureSensorlmpl.java
+++ Abstrakte Klassen/Sensor/main/Barometric1000PressureSensorlmpl.java
@@ -1,8 +1,9 @@
-public class Barometric1000OressureSensorImpl extends PressureSensor{
+public class Barometric1000OressureSensorImpl extends PressureSensor {
 
     public void doMeasurement() {
         double max = 1.05;
         double min = 0.5;
         double range = max - min;
         measurementValue = (Math.random() * range) + min;
-        }
+    }
+}
