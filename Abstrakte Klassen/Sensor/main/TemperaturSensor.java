2db5438b86c544628f2ea4ea1dc21e33d0eeac59
Safe6er69 <kurtajleon533@gmail.com>
2023-12-12 14:57:51 +0100

    added value super(unit, value, name)

--- Abstrakte Klassen/Sensor/main/TemperaturSensor.java
+++ Abstrakte Klassen/Sensor/main/TemperaturSensor.java
@@ -4,7 +4,7 @@ public abstract class TemperaturSensor extends Sensor {
     protected double measurementValue = 69;
 
     public TemperaturSensor(String unit, double value, String name){
-        super();
+        super(unit, value, name);
 
     }
 
